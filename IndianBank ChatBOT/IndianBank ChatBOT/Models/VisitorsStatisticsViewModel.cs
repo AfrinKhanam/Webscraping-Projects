2de778c9b3f16362729a04202a38c4180aa40728
Lakshmana BP <lakshmanabp@botaiml.com>
2020-02-13 13:34:42 +0530

    Real time dashboard API chanegs.

---
+++ IndianBank ChatBOT/IndianBank ChatBOT/Models/VisitorsStatisticsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndianBank_ChatBOT.Models
+{
+    public class VisitorsStatisticsViewModel
+    {
+        public int NumberOfVisitsCurrentYear { get; set; }
+        public int NumberOfVisitsLastYear { get; set; }
+        public int NumberOfVisitsCurrentMonth { get; set; }
+        public int NumberOfVisitsLastMonth { get; set; }
+        public int NumberOfVisitsToday { get; set; }
+        public int NumberOfVisitsYesterday { get; set; }
+
+        public int NumberOfUnsatisfactoryVisitsCurrentYear { get; set; }
+        public int NumberOfUnsatisfactoryVisitsLastYear { get; set; }
+        public int NumberOfUnsatisfactoryVisitsCurrentMonth { get; set; }
+        public int NumberOfUnsatisfactoryVisitsLastMonth { get; set; }
+        public int NumberOfUnsatisfactoryVisitsToday { get; set; }
+        public int NumberOfUnsatisfactoryVisitsYesterday { get; set; }
+    }
+}
