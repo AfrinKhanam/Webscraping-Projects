b8419ddbdda025384c0956b3dc02fc566373f6bb
Lakshmana BP <lakshmanabp@botaiml.com>
2020-05-06 15:34:32 +0530

    Updated the FrameAncestors to None

--- IndianBank ChatBOT/IndianBank ChatBOT/Startup.cs
+++ IndianBank ChatBOT/IndianBank ChatBOT/Startup.cs
@@ -181,7 +181,7 @@ namespace IndianBank_ChatBOT
             app.UseXfo(options => options.Deny());
             app.UseCsp(opts => opts
             .BlockAllMixedContent()
-            .FrameAncestors(s => s.Self())
+            .FrameAncestors(s => s.None())
             );
 
 
