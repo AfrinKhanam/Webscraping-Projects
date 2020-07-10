9f5c8d37169f78713fc8b23bc71f32e826f77b5e
Ashutosh <ashutoshkumar@integramicro.com>
2020-07-10 22:27:10 +0530

    rescrape urls api and rescrape by urls  api designcompleted

--- auto-suggest-question/elastic.py
+++ auto-suggest-question/elastic.py
@@ -2,9 +2,10 @@ from elasticsearch import Elasticsearch
 import json
 from uuid import uuid1
 
+
 class Elastic():
     def __init__(self, host='localhost', port='9200', index='indian-bank-auto-suggestion-v2', doc_type='_doc'):
-        self.es = Elasticsearch([{'host':'localhost','port':9200}])
+        self.es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
         self.index = index
         self.doc_type = doc_type
 
@@ -14,34 +15,59 @@ class Elastic():
         print(json.dumps(document, indent=4))
         print('------------------------------------------------------------\n\n')
 
-        self.es.index(index=self.index, doc_type=self.doc_type, id=str(uuid1()), body=document)
+        self.es.index(index=self.index, doc_type=self.doc_type,
+                      id=str(uuid1()), body=document)
         # --------------------------------------------------------- #
 
         return document
 
     def search_with_original_query(self, search_string, context):
-       #---------------------------------------------------------------#                  
-       print("SEARCH STRING :: ", search_string)
+        #---------------------------------------------------------------#
+        print("SEARCH STRING :: ", search_string)
 
-       es_result = self.es.search(
-           index = self.index,
-           body =
-           {
-               "from" : 0, "size" : 50,
-               "query": {
-                   "bool": {
-                       "should": [
-                           {"match": {"Questions":  search_string} },
-                           {"match": {"primary_context_and_keyword": context} }
-                       ]
-                   }
-               }
-           })
+        es_result = self.es.search(
+            index=self.index,
+            body={
+                "from": 0, "size": 50,
+                "query": {
+                    "bool": {
+                        "should": [
+                            {"match": {"Questions":  search_string}},
+                            {"match": {"primary_context_and_keyword": context}}
+                        ]
+                    }
+                }
+            })
 
-       if len(es_result['hits']['hits']) == 0:
+        if len(es_result['hits']['hits']) == 0:
             return None
-       else:
+        else:
             return es_result
+
+    # def delete_by_condition(self):
+    #     query = {
+    #         "query": {
+    #             "bool": {
+    #                 "should": [
+    #                     {
+    #                         "match_phrase": {
+    #                             "url": "https://indianbank.in/departments/ind-sme-ease/",
+
+    #                         }
+    #                     },
+    #                     {
+    #                         "match_phrase": {
+    #                             "url": "https://www.indianbank.in/departments/ind-sme-ease/",
+
+    #                         }
+    #                     }
+    #                 ]
+    #             }
+    #         }
+    #     }
+
+    #     res = self.es.delete_by_query(index='static_pages', body=query)
+    #     print(json.dumps(res, indent=4))
 
 
 elastic = Elastic()
@@ -53,9 +79,6 @@ with open('./auto-suggestion-question-v2.json') as file:
     for question in question_list:
         elastic.index_document(question)
 
-   
-
-
-
+# elastic.delete_by_condition()
 # result = elastic.search_with_original_query('cold', 'jewel loan')
 # print(json.dumps(result, indent=4))
---
+++ generating-json-structure/rescrape_all_pages/building_blocks/MessageQueue/rabbitmq_pipe.py
@@ -0,0 +1,71 @@
+import pika
+from configparser import ConfigParser
+
+config_file_path = '../../config.ini'
+config = ConfigParser()
+config.read(config_file_path)
+host = config.get('rabbitmq_webscraping_credentials', 'host')
+username = config.get('rabbitmq_webscraping_credentials', 'username')
+password = config.get('rabbitmq_webscraping_credentials', 'password')
+vhost = config.get('rabbitmq_webscraping_credentials', 'vhost')
+class RabbitmqProducerPipe:
+    def __init__(self, publish_exchange, routing_key, queue_name, host=host):
+
+        self.credentials = pika.PlainCredentials(username, password)
+        self.publish_routing_key = routing_key
+        self.publish_exchange = publish_exchange
+        self.queue_name = queue_name
+
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host, 
+            virtual_host=vhost,
+            heartbeat = 0,
+            credentials=self.credentials))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=publish_exchange, exchange_type='direct', durable=True)
+
+        self.channel.queue_declare(self.queue_name, durable=True)
+        self.channel.queue_bind(exchange=self.publish_exchange, queue=self.queue_name, routing_key=self.publish_routing_key)
+
+
+    def publish(self, message):
+        self.channel.basic_publish(
+            exchange=self.publish_exchange, 
+            routing_key=self.publish_routing_key, 
+            body=message,
+            properties=pika.BasicProperties(delivery_mode=2)
+            )
+
+    def close(self):
+        self.connection.close()
+
+    
+class RabbitmqConsumerPipe:
+    def __init__(self, exchange, queue, routing_key, callback, host=host):
+        self.queue = queue
+        self.exchange = exchange
+        self.routing_key = routing_key
+        self.credentials = pika.PlainCredentials(username, password)
+
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host, 
+            virtual_host=vhost,
+            heartbeat=0,
+            credentials=self.credentials))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=self.exchange, exchange_type='direct',durable=True)
+        self.channel.queue_declare(self.queue, durable=True)
+        self.channel.queue_bind(exchange=self.exchange, queue=self.queue, routing_key=self.routing_key)
+
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue=self.queue, on_message_callback=callback, auto_ack=True)
+
+    def start_consuming(self):
+        self.channel.start_consuming()
+    
+
+
+
+
+
+
