9110c9a7376f1a753fe8a89ff37c1e548037cff0
AfreenKhanam <afrin@botaiml.com>
2020-07-24 02:18:57 +0530

    completed scraping of home loan page and ib clean loan page

--- simplified-qa-pipeline/webscraping/post_processing_functions/clean_loan.py
+++ simplified-qa-pipeline/webscraping/post_processing_functions/clean_loan.py
@@ -1,6 +1,6 @@
 def clean_loan(document):
-    record = document['subtitle']['elements'][0]['content'][0]['table'][2]['value'][0]['table']['values']
+    record = document['subtitle']['elements'][0]['content'][0]['table'][3]
+    record['key'] = document['subtitle']['elements'][0]['content'][0]['table'][3]['key']
+    record['value'] = document['subtitle']['elements'][0]['content'][0]['table'][3]['value'][0]['table']['values'][0]
+    return document 
 
-    record[2].append(record[1][2])
-
-    return document
