668b73e78dda7b205b541e7b6f7c509af92ec489
AfreenKhanam <afrin@botaiml.com>
2020-08-28 10:51:30 +0530

    done bug fix to handle "cdo" query in "GM" page

--- nlp-qa-api/search/doc_prioritizer.py
+++ nlp-qa-api/search/doc_prioritizer.py
@@ -871,7 +871,7 @@ class DocPrioritizer:
             document['ES_RESULT']['DOCUMENTS'] = documents[0:3]
 
         # ['gm ',' gm','gm','cro','cdo','ra','clo',' bi','coo','r&gr','i&c']
-        if re.search(r'((\sfgm$)|(^fgm\s)|(\sfgm\s)|(^fgm$)|(\sfgm\?$)|(\scro$)|(^cro\s)|(\scro\s)|(^cro$)|(\scro\?$)|(\sgm$)|(^gm\s)|(\sgm\s)|(^gm$)|(\sgm\?$)|(\sra$)|(^ra\s)|(\sra\s)|(^ra$)|(\sra\?$)|(\sclo$)|(^clo\s)|(\sclo\s)|(^clo$)|(\sclo\?$)|(\sbi$)|(^bi\s)|(\sbi\s)|(^bi$)|(\sbi\?$)|(\scoo$)|(^coo\s)|(\scoo\s)|(^coo$)|(\scoo\?$)|(\sr&gr$)|(^r&gr\s)|(\sr&gr\s)|(^r&gr$)|(\sr&gr\?$) |(\si&c$)|(^i&c\s)|(\si&c\s)|(^i&c$)|(\si&c\?$))',document['POTENTIAL_QUERY_LIST']):
+        if re.search(r'((\scdo$)|(^cdo\s)|(\scdo\s)|(^cdo$)|(\scdo\?$)|(\sfgm$)|(^fgm\s)|(\sfgm\s)|(^fgm$)|(\sfgm\?$)|(\scro$)|(^cro\s)|(\scro\s)|(^cro$)|(\scro\?$)|(\sgm$)|(^gm\s)|(\sgm\s)|(^gm$)|(\sgm\?$)|(\sra$)|(^ra\s)|(\sra\s)|(^ra$)|(\sra\?$)|(\sclo$)|(^clo\s)|(\sclo\s)|(^clo$)|(\sclo\?$)|(\sbi$)|(^bi\s)|(\sbi\s)|(^bi$)|(\sbi\?$)|(\scoo$)|(^coo\s)|(\scoo\s)|(^coo$)|(\scoo\?$)|(\sr&gr$)|(^r&gr\s)|(\sr&gr\s)|(^r&gr$)|(\sr&gr\?$) |(\si&c$)|(^i&c\s)|(\si&c\s)|(^i&c$)|(\si&c\?$))',document['POTENTIAL_QUERY_LIST']):
             if not re.search(r'((\scvo$)|(^cvo\s)|(\scvo\s)|(^cvo$)|(\scvo\?$))',document['POTENTIAL_QUERY_LIST']):
                 for record in document['ES_RESULT']['DOCUMENTS']:
                     if record['url'] == 'https://www.indianbank.in/departments/general-managers/':
